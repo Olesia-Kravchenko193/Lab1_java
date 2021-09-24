a58194a9981a7c144b0216f77416b19863bd4028
Olesia-Kravchenko193 <yaseloklass@gmail.com>
2021-09-24 23:12:03 +0300

    исправления

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,21 @@
 package com.company;
 import java.util.Scanner;
+import java.time.LocalDate;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.time.ZoneId;
+import java.time.Period;
 
 public class Main {
 
     public static void main(String[] args) {
         Task1();
+        /*Task2();*/
     }
 
+    /* Вариант 14
+    Определить товар, количество которого больше всего
+    на складе, и напечатать все сведения о нем.*/
         public static void Task1() {
 
             Scanner scanner = new Scanner(System.in);
@@ -42,11 +51,65 @@ public class Main {
                     max = i + 1;
                 }
             }
-            System.out.print("Название:" + array_1[max].name);
+            System.out.print("\nНазвание:" + array_1[max].name);
+            System.out.print("\nКол-во:" + array_1[max].number);
+            System.out.print("\nЦена:" + array_1[max].price);
+            System.out.print("\nГод изготовления:" + array_1[max].year);
+            System.out.print("\nПроизводитель:" + array_1[max].manufacturer);
+        }
+
+        /* Вариант 14
+         Вывести все сведения о сотрудниках, стаж которых
+         превышает 10 лет.*/
+        public static void Task2() {
+
+            Scanner scanner = new Scanner(System.in);
+            int size;
+            do {
+                System.out.print("Введите кол-во сотрудников: ");
+                size = scanner.nextInt();
+                scanner.nextLine();
+            } while (size <= 0);
+            Employee[] array_2 = new Employee[size];
+            int []arrayYear=new int[size];
+            double[] arraySPEED = new double[size];
+            double[] arrayTIMES = new double[size];
+
+            for (int i = 0; i < size; i++) {
+                array_2[i] = new Employee();
+                System.out.print("Фамилия: ");
+                array_2[i].surname = scanner.nextLine();
+                System.out.print("Имя: ");
+                array_2[i].name = scanner.nextLine();
+                System.out.print("Отчество: ");
+                array_2[i].patronymic = scanner.nextLine();
+                System.out.print("Должность: ");
+                array_2[i].position = scanner.nextLine();
+                scanner.nextLine();
+                System.out.print("Пол: ");
+                array_2[i].gender = scanner.nextLine();
+
+                LocalDate date = LocalDate.now(); // получаем текущую дату
+                System.out.println(date);
+                arraySPEED[i] = (array_2[i].date / arrayTIMES[i]);
+                System.out.print("Дата приема на работу: ");
+                array_2[i].date=scanner.nextInt();
+                Date DATE_1=new SimpleDateFormat("dd/MM/yyyy").parse(array_2[i].date);
+                LocalDate date11 = DATE_1.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+                Period periodYear=Period.between(date11,date);
+                arrayYear[i]=periodYear.getYears();
+                /*System.out.print("Дата приема на работу: ");
+                array_2[i].date = scanner.nextInt();*/
+
+            }
+
+
+            /*System.out.print("Название:" + array_1[max].name);
             System.out.print("Кол-во:" + array_1[max].number);
             System.out.print("Цена:" + array_1[max].price);
             System.out.print("Год изготовления:" + array_1[max].year);
-            System.out.print("Производитель:" + array_1[max].manufacturer);
+            System.out.print("Производитель:" + array_1[max].manufacturer);*/
         }
+
 }
 
